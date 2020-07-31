df4ca7e7cbbd95b43615761d4c994252bd8fb05f
peace <astonreba@gmail.com>
2020-07-31 13:50:37 +0300

    Work with cookies

--- Flask-full/app.py
+++ Flask-full/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
@@ -9,6 +9,20 @@ def index():
 @app.route('/user/<username>')
 def user(username):
     return f'Hello there {username}'
+
+# How to work with cookies
+
+@app.route('/set')
+def setCookie():
+    response = make_response('I have set the cookie')
+    response.set_cookie('myapp', 'Flask Development')
+
+    return response
+
+@app.route('/get')
+def getCookie():
+    myapp = request.cookies.get('myapp')
+    return 'Cookie content ' + str(myapp)
 
 if __name__ == "__main__":
     app.run(debug=True)

d71d3d2f83b811374186365471dd2cec68272a09
peace <astonreba@gmail.com>
2020-07-31 13:37:25 +0300

    Modify file

--- Flask-full/app.py
+++ Flask-full/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Apple Lurve'
 
+@app.route('/user/<username>')
+def user(username):
+    return f'Hello there {username}'
+
 if __name__ == "__main__":
     app.run(debug=True)
